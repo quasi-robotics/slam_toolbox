f5c4adc3d811a60f44c63dedac3e0608557cca83
Quasi Developer <info@quasi.ai>
2025-10-28 15:45:36 -0400

    resolved merge errors

--- launch/localization_launch.py
+++ launch/localization_launch.py
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 import os
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-=======
-
-import os
-
->>>>>>> upstream/ros2
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
